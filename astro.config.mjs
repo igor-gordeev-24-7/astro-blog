3236f32f6eaf9e32df94fa2bd27f56caf987951c
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2025-11-29 18:28:14 +0300

    Добавил Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rhymes-and-morgen.ru/"
+  site: "https://rhymes-and-morgen.ru/",
+  integrations: [preact()]
 });
